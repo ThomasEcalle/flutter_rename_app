77cac660bfebb2dfdb513d9d069fb5690d88fb13
Ecalle Thomas <thomasecalle@hotmail.fr>
2019-12-30 20:49:20 +0100

    Remove example app to clean a bit

--- example/lib/screens/home.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:shoppy_app/widgets/my_widget.dart';
-
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MyWidget();
-  }
-}
