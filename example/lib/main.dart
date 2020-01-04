1bf94000f5d7b4471dbbaeb5a7c6b724966347a5
Ecalle Thomas <thomasecalle@hotmail.fr>
2020-01-04 02:02:14 +0100

    Create new example app

---
+++ example/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:example/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
---
+++ example/lib/screens/home.dart
@@ -0,0 +1,11 @@
+import 'package:example/widgets/awesome_widget.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AwesomeWidget(),
+    );
+  }
+}
